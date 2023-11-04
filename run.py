ea9fee8869cf55e80ba44dbf13ea7b79978eca1c
JesseRoss001 <>
2023-11-04 01:53:40 +0000

    Refractored updating surplus and sales functions into one called update_worksheet

--- run.py
+++ run.py
@@ -53,24 +53,26 @@ def validate_data(values):
         return False
     
     return True 
-
+"""
 def update_sales_worksheet(data):
-    """
-    Update sales worksheet and new row with the list data provided
-    """
+   
     print("Updating sales work .... \n")
     sales_worksheet =SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
     print("sales worksheet updated successfully \n")
 
 def update_surplus_worksheet(data):
-    """
-    Update surplus worksheet and new row with the list data provided
-    """
+
     print("Updating surplus work .... \n")
     surplus_worksheet =SHEET.worksheet("surplus")
     surplus_worksheet.append_row(data)
     print("surplus worksheet updated successfully \n")
+"""
+def update_worksheet(data,worksheet):
+    print(f"Updating {worksheet} worksheet ... \n")
+    worksheet_to_update =SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated sucessfully \n ")
 
 def calculate_surplus_data(sales_row):
     """
@@ -94,9 +96,9 @@ def main():
 
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data,"sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data,"surplus")
 print("Welcome to love sandwiches data automation")
 main()
 
